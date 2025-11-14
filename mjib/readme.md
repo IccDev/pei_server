07ffc070f0db20b1bad310a3a2dabfaa32764d6a
djedou <djedouarnaud.aman@gmail.com>
2025-11-14 13:07:38 +0100

    mjib

--- mjib/readme.md
+++ mjib/readme.md
@@ -14,7 +14,11 @@
 docker login -u iccbrx -p IccDev@2023
 
 docker tag c8aebb393ab1 iccbrx/icc_identity_system:dev
+docker tag 0bc78f828485 iccbrx/mjib:dev
+
+
 
 docker push iccbrx/icc_identity_system:dev
+docker push iccbrx/mjib:dev
 
 
--- mjib/src/routes/queries/mod.rs
+++ mjib/src/routes/queries/mod.rs
@@ -5,45 +5,45 @@ use crate::database::Database;
 
 pub fn get_sections() -> impl Reply {
     let mut db = DB.access();
-    Response::new(format!("{:#?}", DB.get_sections(&mut db)).into())
+    Response::new(DB.get_sections(&mut db).map_or("".to_string(), |d| format!("{d:#?}")).into())
 }
 
 pub fn get_section_by_id(id: i32) -> impl Reply {
     let mut db = DB.access();
-    Response::new(format!("{:#?}", DB.get_section_by_id(id, &mut db)).into())
+    Response::new(DB.get_section_by_id(id, &mut db).map_or("".to_string(), |d| format!("{d:#?}")).into())
 }
 
 pub fn get_discipline_by_id(id: i32) -> impl Reply {
     let mut db = DB.access();
-    Response::new(format!("{:#?}", DB.get_discipline_by_id(id, &mut db)).into())
+    Response::new(DB.get_discipline_by_id(id, &mut db).map_or("".to_string(), |d| format!("{d:#?}")).into())
 }
 
 pub fn get_discipline_by_section_id(id: i32) -> impl Reply {
     let mut db = DB.access();
-    Response::new(format!("{:#?}", DB.get_discipline_by_section_id(id, &mut db)).into())
+    Response::new(DB.get_discipline_by_section_id(id, &mut db).map_or("".to_string(), |d| format!("{d:#?}")).into())
 }
 
 pub fn get_disciplines() -> impl Reply {
     let mut db = DB.access();
-    Response::new(format!("{:#?}", DB.get_disciplines(&mut db)).into())
+    Response::new(DB.get_disciplines(&mut db).map_or("".to_string(), |d| format!("{d:#?}")).into())
 }
 
 pub fn get_courses() -> impl Reply {
     let mut db = DB.access();
-    Response::new(format!("{:#?}", DB.get_courses(&mut db)).into())
+    Response::new(DB.get_courses(&mut db).map_or("".to_string(), |d| format!("{d:#?}")).into())
 }
 
 pub fn get_course_by_id(id: i32) -> impl Reply {
     let mut db = DB.access();
-    Response::new(format!("{:#?}", DB.get_course_by_id(id, &mut db)).into())
+    Response::new(DB.get_course_by_id(id, &mut db).map_or("".to_string(), |d| format!("{d:#?}")).into())
 }
 
 pub fn get_course_disciplines_by_course_id(id: i32) -> impl Reply {
     let mut db = DB.access();
-    Response::new(format!("{:#?}", DB.get_course_disciplines_by_course_id(id, &mut db)).into())
+    Response::new(DB.get_course_disciplines_by_course_id(id, &mut db).map_or("".to_string(), |d| format!("{d:#?}")).into())
 }
 
 pub fn get_course_disciplines() -> impl Reply {
     let mut db = DB.access();
-    Response::new(format!("{:#?}", DB.get_course_disciplines(&mut db)).into())
+    Response::new(DB.get_course_disciplines(&mut db).map_or("".to_string(), |d| format!("{d:#?}")).into())
 }
